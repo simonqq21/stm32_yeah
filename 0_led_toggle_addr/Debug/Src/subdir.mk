a9c874c2afa2e9585bb1020267bec1f6cf50ba29
SImon QUe <ques86225@gmail.com>
2026-07-02 22:18:37 +0800

    initial commit - learn STM32 bare metal programming

---
+++ 0_led_toggle_addr/Debug/Src/subdir.mk
@@ -0,0 +1,31 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Src/main.c 
+
+C_DEPS += \
+./Src/main.d 
+
+OBJS += \
+./Src/main.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Src/%.o: ../Src/%.c Src/subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross GCC Compiler'
+	gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
+clean: clean-Src
+
+clean-Src:
+	-$(RM) ./Src/main.d ./Src/main.o
+
+.PHONY: clean-Src
+
